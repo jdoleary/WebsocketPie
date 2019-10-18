5fbed1ea42b3df02cab87cab76056c1f7fbfc170
Jordan O'Leary <jdoleary@gmail.com>
2019-10-18 07:24:38 -0400

    feat: Set up main methods
    Abstracts finalized echo server api in a client package

---
+++ PieClient/index.js
@@ -0,0 +1,83 @@
+const WebSocket = require('ws');
+
+/*
+env: 'development' | 'production'
+wsUri: websocket uri of an echo server instance
+ws: websocket connection
+onData: a callback that is send data emitted by the echo server
+onInfo: a callback to send information about the connection
+onError: a callback to send error messages
+*/
+
+const env = {
+  development: 'development',
+  production: 'production',
+};
+class PieClient {
+  constructor({ env, wsUri, onData, onInfo, onError }) {
+    this.env = env;
+    this.wsUri = wsUri;
+    this.onData = onData;
+    this.onInfo = onInfo;
+    this.onError = onError;
+    this.connected = false;
+
+    this.ws = new WebSocket(wsUri);
+    this.ws.on('message', data => {
+      try {
+        const message = JSON.parse(data);
+        this.onData(message);
+      } catch (e) {
+        this.onError(e);
+      }
+    });
+    this.ws.on('open', () => {
+      this.connected = true;
+      this.onInfo({ connected: this.connected, msg: `Opened connection to ${this.wsUri}` });
+    });
+    this.ws.on('close', () => {
+      this.connected = false;
+      this.onInfo({ connected: this.connected, msg: `Connection to ${this.wsUri} closed.` });
+    });
+  }
+  joinRoom(roomInfo) {
+    if (this.connected) {
+      this.ws.send({
+        type: 'joinRoom',
+        roomInfo,
+      });
+    } else {
+      this.onError({ msg: `Cannot join room, not currently connected to web socket server` });
+    }
+  }
+  leaveRoom() {
+    if (this.connected) {
+      this.ws.send({
+        type: 'leaveRoom',
+      });
+    } else {
+      this.onError({ msg: `Cannot leave room, not currently connected to web socket server` });
+    }
+  }
+  getRooms(roomInfo) {
+    if (this.connected) {
+      this.ws.send({
+        type: 'getRooms',
+        roomInfo,
+      });
+    } else {
+      this.onError({ msg: `Cannot get rooms, not currently connected to web socket server` });
+    }
+  }
+  sendData(payload) {
+    if (this.connected) {
+      this.ws.send({
+        type: 'data',
+        payload,
+      });
+    } else {
+      this.onError({ msg: `Cannot send data to room, not currently connected to web socket server` });
+    }
+  }
+}
+module.exports = PieClient;
---
+++ PieClient/readme.md
@@ -0,0 +1,5 @@
+# Pie Client
+
+A client for connecting to, sending and receiving messages from an echo server instance.
+
+This package is an echo server client that abstracts echo server's public API behind simple publicly exported methods.
