76bcadb7a982fd7a93be7661a94045aae0c6a7f0
Jordan O'Leary <jdoleary@gmail.com>
2020-04-05 09:43:16 -0400

    log: Always log server start info

--- packages/PieServer/src/index.js
+++ packages/PieServer/src/index.js
@@ -1,7 +1,6 @@
-const log = require('./log');
 const { startServer } = require('./network');
 
 const version = process.env.npm_package_version;
-log(`Running Echo Server v${version}.`);
 const port = process.env.PORT || 8080;
+console.log(`Running Echo Server v${version} with port ${port}`);
 startServer({ port });
