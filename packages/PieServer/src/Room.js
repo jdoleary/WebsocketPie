jdoleary/WebsocketPie

commit fe3b2227c22d97df19747fdae3174ab1ad2eed1d

fix: Remove client from their previous room
when joining a new room ONLY after the new room join is successful.
This fixes an issue where if a client tried to join the same room
in two different tabs with the same clientId, it
would reject the join but also disconnect the first connection
because it would remove them from their current room thinking they
were about to join another room.

packages/PieServer/src/RoomManager.js
@@ -50,9 +50,10 @@ class RoomManager {
     if (!(client && roomInfo)) {
       return Promise.reject('Cannot add client to room, missing "client", and/or "roomInfo"');
     }
-    if (client.room) {
-      this.removeClientFromCurrentRoom(client);
-    }
+    // Cache the clients previous room,
+    // then later in this function if the client successfully joins
+    // the new room, remove the client from their old room
+    const previousRoom = client.room;
     const room = this.getRoom(roomInfo);
     if (!room) {
       return Promise.reject(`Cannot add client to room, unable to find the preexisting room`);
@@ -69,10 +70,16 @@ class RoomManager {
     );
     client = Object.assign(client, { room });
     try {
-    room.addClient(client);
+      room.addClient(client);
     } catch (e) {
       return Promise.reject(e.toString());
     }
+
+    // Remove the client from their previous room if applicable
+    if (previousRoom) {
+      this.removeClientFromRoom(client, previousRoom);
+    }
+
     return Promise.resolve(room);
   }
 
@@ -98,12 +105,11 @@ class RoomManager {
     }
   }
 
-  removeClientFromCurrentRoom(client) {
-    const { room } = client;
+  removeClientFromRoom(client, room) {
     if (!room) {
       return;
     }
-    log(chalk.blue(`Removing client ${client.id} from current room`));
+    log(chalk.blue(`Removing client ${client.id} from room ${room.toString()}`));
     room.removeClient(client);
     // Remove data added while joining room.
     delete client.room;

packages/PieServer/src/network.js
@@ -61,7 +61,7 @@ function startServer({ port, heartbeatCheckMillis = 5000, makeHostAppInstance = 
             roomManager.onData({ client, message });
             break;
           case MessageType.LeaveRoom:
-            roomManager.removeClientFromCurrentRoom(client);
+            roomManager.removeClientFromRoom(client, client.room);
             break;
           case MessageType.GetRooms:
             roomManager.getRooms({ client, roomInfo: message.roomInfo });
@@ -81,7 +81,7 @@ function startServer({ port, heartbeatCheckMillis = 5000, makeHostAppInstance = 
     });
     client.on('close', () => {
       log(chalk.blue(`Client ${client.id} disconnected`));
-      roomManager.removeClientFromCurrentRoom(client);
+      roomManager.removeClientFromRoom(client, client.room);
     });
     function resolveClientPromise(func, data) {
       client.send(

commit 7bbc673f9e288f48110238991d9fd51c9b822e17

fix: Prevent client joining multiple rooms with error message rather than automatically leaving previous room. fe3b222 introduced a bug because removeClientFromRoom also deletes client.room info but I had invoked removeClientFromRoom(client, previousRoom) after they had joined the new room so it was both removing them from the previous room and also deleting the current room info from the client object. That whole flow was too convoluted so I've decided it's better to just return a rejected promise if a client tries to join a new room without first leaving their previous room.

packages/PieServer/src/Room.js
@@ -131,7 +131,7 @@ class Room {
   addClient(client) {
     const clientIndex = this.getClientIndex(client);
     if (clientIndex !== -1) {
-      throw new Error('Cannot add client, client is already in the room');
+      throw new Error('Cannot readd client to room; the client is already in the room');
     }
     if (this.maxClients !== undefined && this.clients.length === this.maxClients) {
       throw new Error(`Room is at capacity and cannot accept more clients due to the room's chosen settings`);

packages/PieServer/src/RoomManager.js
@@ -51,13 +51,12 @@ class RoomManager {
       if (!(client && roomInfo)) {
         return Promise.reject('Cannot add client to room, missing "client", and/or "roomInfo"');
       }
-      // Cache the clients previous room,
-      // then later in this function if the client successfully joins
-      // the new room, remove the client from their old room
-      const previousRoom = client.room;
+      if (client.room) {
+        return Promise.reject('Client cannot join a new room without first leaving their current room.');
+      }
       const room = this.getRoom(roomInfo);
       if (!room) {
-        return Promise.reject(`Cannot add client to room, unable to find the preexisting room`);
+        return Promise.reject(`Cannot add client to non-existant room.`);
       }
       if (room.cleanupTimeoutId !== undefined) {
         log(chalk.blue(`Cancelling clean up for room ${room.toString()} because a client has rejoined.`));
@@ -71,11 +70,6 @@ class RoomManager {
       );
       client = Object.assign(client, { room });
       room.addClient(client);
-
-      // Remove the client from their previous room if applicable
-      if (previousRoom) {
-        this.removeClientFromRoom(client, previousRoom);
-      }
       return Promise.resolve(room);
     } catch (e) {
       // This function should always return a promise, even if it throws