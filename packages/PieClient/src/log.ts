eea99ba3a7a1eff03f430260ec6e3933d1aab599
Jordan O'Leary <jdoleary@gmail.com>
2023-02-22 10:46:11 -0500

    log: Improve error logging for Sentry.io
    so that it keeps the context

--- packages/PieClient/src/log.ts
+++ packages/PieClient/src/log.ts
@@ -2,5 +2,10 @@ export function log(...args: any[]) {
     console.log('websocket🥧:', ...args);
 }
 export function logError(...args: any[]) {
+    try {
+        console.log('websocket🥧: error context: ', JSON.stringify(args));
+    } catch (e) {
+        // Ignore, if stringify fails, just don't log the extra context
+    }
     console.error('websocket🥧:', ...args);
 }
