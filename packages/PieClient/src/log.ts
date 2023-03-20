e1cfd9c2dce14e5e189612596af46d17bc1b6cd3
Jordan O'Leary <jdoleary@gmail.com>
2023-03-20 10:34:17 -0400

    log: Add trace to wsPie error
    in case stringify fails for the sake of viewing on
    Sentry

--- packages/PieClient/src/log.ts
+++ packages/PieClient/src/log.ts
@@ -3,6 +3,7 @@ export function log(...args: any[]) {
 }
 export function logError(...args: any[]) {
     try {
+        console.trace('websocket🥧: error trace');
         console.log('websocket🥧: error context: ', JSON.stringify(args));
     } catch (e) {
         // Ignore, if stringify fails, just don't log the extra context
