5f5e4e65bc93e6e61a3a12e4dd3cdfd66e028a1c
Jordan O'Leary <jdoleary@gmail.com>
2023-10-22 21:57:23 -0400

    ref: PieServer to use Bun runtime

--- packages/PieServer/src/network.js
+++ packages/PieServer/src/network.js
@@ -19,6 +19,24 @@ function heartbeat() {
 let roomManager;
 let serverRunningSince = 0;
 let areStatsAllowed = false;
+function resolveClientPromise(client, func, data) {
+  client.send(
+    JSON.stringify({
+      type: MessageType.ResolvePromise,
+      func,
+      data,
+    }),
+  );
+}
+function rejectClientPromise(client, func, err) {
+  client.send(
+    JSON.stringify({
+      type: MessageType.RejectPromise,
+      func,
+      err,
+    }),
+  );
+}
 // makeHostApp: See examples/HostApp/readme.md for explanation about how hostApp works
 function startServer({ port, heartbeatCheckMillis = 5000, makeHostAppInstance = null, allowStats = false }) {
   log(`Running PieServer v${version} with port ${port}.  Stats allowed: ${allowStats}`);
@@ -26,124 +44,122 @@ function startServer({ port, heartbeatCheckMillis = 5000, makeHostAppInstance = 
   // Get the version of the host app so it can be sent to the client on connection
   const hostAppVersion = makeHostAppInstance ? makeHostAppInstance().version : undefined;
   roomManager = new RoomManager(makeHostAppInstance);
-  const webSocketServer = new WebSocket.Server({ port });
-  webSocketServer.on('connection', (client, req) => {
-    client.isAlive = true;
-    client.on('pong', heartbeat);
-    const queryString = parseQueryString(req.url);
-    // Allow user to request a clientId when they join
-    // This supports rejoining after a disconnect
-    const clientId = queryString.clientId || uuidv4();
-    client = Object.assign(client, { id: clientId });
-    log(chalk.blue(`Client ${clientId} connected`));
-    client.send(
-      JSON.stringify({
-        type: MessageType.ServerAssignedData,
-        clientId,
-        // The @websocketpie/server version
-        serverVersion: version,
-        // The version of the optional hostApp
-        hostAppVersion: hostAppVersion
-      }),
-    );
-    client.on('message', data => {
-      log(chalk.blue(`Client ${client.id} sent a message`));
-      try {
-        const message = JSON.parse(data);
-        switch (message.type) {
-          case MessageType.JoinRoom: {
-            const { makeRoomIfNonExistant, roomInfo } = message;
-            if (makeRoomIfNonExistant) {
-              roomManager.makeRoom(roomInfo);
-            }
-            roomManager
-              .addClientToRoom({ client, roomInfo })
-              .then(room => {
-                resolveClientPromise(message.type, room.serialize());
-              })
-              .catch(err => {
-                rejectClientPromise(message.type, err);
-              });
-            break;
-          }
-          case MessageType.Data:
-            roomManager.onData({ client, message });
-            break;
-          case MessageType.LeaveRoom:
-            roomManager.removeClientFromRoom(client, client.room);
-            break;
-          case MessageType.GetRooms:
-            roomManager.getRooms({ client, roomInfo: message.roomInfo });
-            break;
-          case MessageType.GetStats:
-            getStats().then(stats => {
-              client.send(
-                JSON.stringify({
-                  type: MessageType.GetStats,
-                  stats,
-                }),
-              );
-            }).catch(err => {
-              console.error(err);
-              client.send(
-                JSON.stringify({
-                  type: MessageType.Err,
-                  message: err.message,
-                }),
-              );
-            })
-            break;
-          default:
-            throw new Error(`WARN: Message not understood: ${JSON.stringify(message, null, 2)}`);
-        }
-      } catch (err) {
-        console.error('network.js | ', err);
+  const webSocketServer = Bun.serve({
+    port,
+    fetch(req, server){
+      const queryString = parseQueryString(req.url);
+      const success = server.upgrade(req, queryString ? {data:{clientId:queryString.clientId}}: undefined);
+      if (success) {
+        // Bun automatically returns a 101 Switching Protocols
+        // if the upgrade succeeds
+        return undefined;
+      }
+    },
+    websocket: {
+      async open(client){
+        client.isAlive = true;
+        // TODO: fix in bun implementation
+        // client.on('pong', heartbeat);
+
+        // Allow user to request a clientId when they join
+        // This supports rejoining after a disconnect
+        const clientId = client.data && client.data.clientId ? client.data.clientId : uuidv4();
+        client = Object.assign(client, { id: clientId });
+        log(chalk.blue(`Client ${clientId} connected`));
         client.send(
           JSON.stringify({
-            type: MessageType.Err,
-            message: err.message,
+            type: MessageType.ServerAssignedData,
+            clientId,
+            // The @websocketpie/server version
+            serverVersion: version,
+            // The version of the optional hostApp
+            hostAppVersion: hostAppVersion
           }),
         );
-      }
-    });
-    client.on('close', () => {
-      log(chalk.blue(`Client ${client.id} disconnected`));
-      roomManager.removeClientFromRoom(client, client.room);
-    });
-    function resolveClientPromise(func, data) {
-      client.send(
-        JSON.stringify({
-          type: MessageType.ResolvePromise,
-          func,
-          data,
-        }),
-      );
-    }
-    function rejectClientPromise(func, err) {
-      client.send(
-        JSON.stringify({
-          type: MessageType.RejectPromise,
-          func,
-          err,
-        }),
-      );
-    }
-  });
-  const heartbeatInterval = setInterval(function ping() {
-    webSocketServer.clients.forEach(function each(client) {
-      log('Send ping to clients');
-      if (client.isAlive === false) return client.terminate();
+      },
+      async message(client, data){
+        log(chalk.blue(`Client ${client.id} sent a message`));
+        try {
+          const message = JSON.parse(data);
+          switch (message.type) {
+            case MessageType.JoinRoom: {
+              const { makeRoomIfNonExistant, roomInfo } = message;
+              if (makeRoomIfNonExistant) {
+                roomManager.makeRoom(roomInfo);
+              }
+              roomManager
+                .addClientToRoom({ client, roomInfo })
+                .then(room => {
+                  resolveClientPromise(client, message.type, room.serialize());
+                })
+                .catch(err => {
+                  rejectClientPromise(client, message.type, err);
+                });
+              break;
+            }
+            case MessageType.Data:
+              roomManager.onData({ client, message });
+              break;
+            case MessageType.LeaveRoom:
+              roomManager.removeClientFromRoom(client, client.room);
+              break;
+            case MessageType.GetRooms:
+              roomManager.getRooms({ client, roomInfo: message.roomInfo });
+              break;
+            case MessageType.GetStats:
+              getStats().then(stats => {
+                client.send(
+                  JSON.stringify({
+                    type: MessageType.GetStats,
+                    stats,
+                  }),
+                );
+              }).catch(err => {
+                console.error(err);
+                client.send(
+                  JSON.stringify({
+                    type: MessageType.Err,
+                    message: err.message,
+                  }),
+                );
+              })
+              break;
+            default:
+              throw new Error(`WARN: Message not understood: ${JSON.stringify(message, null, 2)}`);
+          }
+        } catch (err) {
+          console.error('network.js | ', err);
+          client.send(
+            JSON.stringify({
+              type: MessageType.Err,
+              message: err.message,
+            }),
+          );
+        }
+      },
+      async close(client, code, message){
+        log(chalk.blue(`Client ${client.id} disconnected; code ${code}; message ${message}`));
+        roomManager.removeClientFromRoom(client, client.room);
+      },
+    },
 
-      client.isAlive = false;
-      client.ping();
-    });
-  }, heartbeatCheckMillis);
-
-  webSocketServer.on('close', function close() {
-    clearInterval(heartbeatInterval);
   });
+  // const heartbeatInterval = setInterval(function ping() {
+  //   webSocketServer.clients.forEach(function each(client) {
+  //     log('Send ping to clients');
+  //     if (client.isAlive === false) return client.terminate();
+
+  //     client.isAlive = false;
+  //     client.ping();
+  //   });
+  // }, heartbeatCheckMillis);
+
+  // webSocketServer.on('close', function close() {
+  //   clearInterval(heartbeatInterval);
+  // });
   log(`Websocket server is listening on *:${port} and will check client heartbeat every ${heartbeatCheckMillis} milliseconds.`);
   serverRunningSince = Date.now();
+  webSocketServer.roomManager = roomManager;
   return webSocketServer;
 }
 async function getStats() {
